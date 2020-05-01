dc101f0a4bc71edcb1cbfbaaa8b537b8879c0daf
PatanSanaulla <spatankhan@gmail.com>
2020-05-01 08:36:36 -0400

    adding the code for data preparation for the give images from the robot

---
+++ DataPreparation.py
@@ -0,0 +1,13 @@
+import cv2
+import os
+from Oxford_dataset.ReadCameraModel import ReadCameraModel
+from Oxford_dataset.UndistortImage import UndistortImage
+
+images = []
+fx, fy , cx , cy , Gcamera_image , LUT = ReadCameraModel('./model')
+for fileName in os.listdir('./stereo/centre'):
+    img = cv2.imread(os.path.join('./stereo/centre',fileName),0)
+    color_image = cv2.cvtColor(img, cv2.COLOR_BayerGR2BGR)
+    undistorted_image = UndistortImage(color_image, LUT)
+    greyScale = cv2.cvtColor(undistorted_image, cv2.COLOR_BGR2GRAY)
+    cv2.imwrite(os.path.join('./data',fileName), greyScale)
